6d7cf47c8566595dd408884156fab3dc84283b85
kaushamb <kaushamb@qti.qualcomm.com>
2025-05-28 01:00:10 +0530

    upgrade

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gemini_interaction/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(GeminiApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
