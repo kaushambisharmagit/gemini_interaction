2ac3c517d13a04fde25099df37e6b0912a5a019b
kaushambisharmagit <kaushambisharmacse@gmail.com>
2025-05-28 12:19:44 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,10 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-android { 
-    compileSdk = 34
-    ndkVersion = "27.0.12077973"
-    namespace = "com.example.gemini_interaction"
+android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    namespace = "com.example.gemini_interaction"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -18,14 +16,11 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.gemini_interaction"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
