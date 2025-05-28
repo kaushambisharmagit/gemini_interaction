6da9cfcc2469ebee54e4d1c4f0d447ef2c9009ca
kaushambisharmagit <kaushambisharmacse@gmail.com>
2025-05-28 14:07:40 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     namespace = "com.example.gemini_interaction"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+        sourceCompatibility JavaVersion.VERSION_11
+        targetCompatibility JavaVersion.VERSION_11
+    }
 
     kotlinOptions {
         jvmTarget = "11"
